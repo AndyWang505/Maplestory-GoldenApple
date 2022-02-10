5790de6f195e8b10fd4c263518d047a8175a7184
weiwei-andy <andywang890505@gmail.com>
2022-02-10 18:52:10 +0800

    20220210-update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const buyBtn = document.getElementById("buyBtn"),
     openBtn = document.getElementById("openBtn"),
     openboxBtn = document.getElementById("openboxBtn"),
     blackchipBtn = document.getElementById("blackchipBtn"),
+    resetBtn = document.getElementById("resetBtn"),
+    drawallBtn = document.getElementById("drawallBtn"),
     /* --Props Label-- */
     applesText = document.getElementById("apples"),
     applechipsText = document.getElementById("appleChips"),
@@ -63,7 +65,7 @@ let apples = 0,
 //載入後優先取得json
 window.onload = () => {
     getJSON();
-    alert("小技巧提醒：當點擊「開抽蘋果」按鈕後，可以長按Enter高速水溝。")
+    alert("小技巧提醒：當點擊『開抽蘋果』按鈕後，可以長按Enter高速水溝。\n ＃另外補充了『重置』及『一鍵水溝』，請小心服用。");
 }
 //fetch取得本地json
 let getJSON = () => {
@@ -101,53 +103,56 @@ buyBtn.addEventListener("click", () => {
     applesText.innerHTML = apples;
     spendText.innerHTML = spend;
 });
+//重置
+resetBtn.addEventListener("click", () => {
+    prizeText.innerHTML = "";
+    apples = 0;
+    appleChips = 0;
+    blackChips = 0;
+    goldBox = 0;
+    frenzyTotem = 0;
+    gourd = 0;
+    count = 0;
+    boxCount = 0;
+    spend = 0;
+    blackchips1 = 0;
+    blackchips2 = 0;
+    blackchips3 = 0;
+    blackchips4 = 0;
+    blackchips5 = 0;
+    blackchips6 = 0;
+    blackchips7 = 0;
+    blackchips8 = 0;
+    blackchips9 = 0;
+    blackchips10 = 0;
+    applesText.innerHTML = apples;
+    applechipsText.innerHTML = appleChips;
+    blackchipsText.innerHTML = blackChips;
+    goldboxText.innerHTML = goldBox;
+    gourdText.innerHTML = gourd;
+    frenzytotemText.innerHTML = frenzyTotem;
+    countText.innerHTML = count;
+    boxcountText.innerHTML = boxCount;
+    spendText.innerHTML = spend;
+    blackchips1Text.innerHTML = blackchips1;
+    blackchips2Text.innerHTML = blackchips2;
+    blackchips3Text.innerHTML = blackchips3;
+    blackchips4Text.innerHTML = blackchips4;
+    blackchips5Text.innerHTML = blackchips5;
+    blackchips6Text.innerHTML = blackchips6;
+    blackchips7Text.innerHTML = blackchips7;
+    blackchips8Text.innerHTML = blackchips8;
+    blackchips9Text.innerHTML = blackchips9;
+    blackchips10Text.innerHTML = blackchips10;
+});
+
 openBtn.addEventListener("click", () => {
     if(apples === 0){
         alert("黃金蘋果數量不足，請確認數量是否足夠。");
     }else{
         prizeText.focus;
-        apples -= 1;
-        appleChips += 1;
-        count += 1;
-        console.log("抽了"+count+"目前剩"+apples);
-        //100.00%
-        let probability = Math.round(Math.random()*10000);
-        if(probability <= 6){
-            //輪迴碑石0.06%
-            frenzyTotem += 1;
-            // console.log("最後獎勵為：" + superbigPrize.name);
-            prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${superbigPrize.name}</span>。</h6>`;
-            alert("恭喜從黃金蘋果機獲得【輪迴碑石】！");
-        }else if(probability <= 117){
-            //睿智葫蘆1.11%
-            gourd += 1;
-            // console.log("最後獎勵為：" + verybigPrize.name);
-            prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${verybigPrize.name}</span> 道具1個。</h6>`;
-        }else if(probability <= 808){
-            //漆黑碎片(1) 6.91%
-            blackChips += 1;
-            // console.log("最後獎勵為：" + normalbigPrize.name);
-            prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${normalbigPrize.name}</span>。</h6>`;
-        }else if(probability <= 964){
-            //上廣獎 9.64% 採用 普通皮皮計算上廣機率https://www.youtube.com/watch?v=al8tnzHBBo0&ab_channel=%E6%99%AE%E9%80%9A%E7%9A%AE%E7%9A%AE
-            let bigPrizeLength = bigPrize.length-1;
-            let normalP = Math.round(Math.random() * bigPrizeLength);
-            // console.log(normalP);
-            // console.log("最後獎勵為：" + bigPrize[normalP].name);
-            prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${bigPrize[normalP].name}</span>。</h6>`;
-        }else if(probability <= 3964){
-            //小獎 30% 航海師裝備
-            let smallPrize2Length = bigPrize.length-1;
-            let smallP2  = Math.round(Math.random() * smallPrize2Length);
-            // console.log("最後獎勵為：" + smallPrize2[smallP2].name);
-            prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${smallPrize2[smallP2].name}</span> 道具1個。</h6>`;
-        }else{
-            //一般獎勵
-            let smallPrizeLength = smallPrize.length-1;
-            let smallP  = Math.round(Math.random() * smallPrizeLength);
-            // console.log("最後獎勵為：" + smallPrize[smallP].name);
-            prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${smallPrize[smallP].name}</span> 道具1個。</h6>`;
-        }
+        console.log("抽獎進行"+count+"次，剩"+apples);
+        prizeDraw();
         applesText.innerHTML = apples;
         applechipsText.innerHTML = appleChips;
         blackchipsText.innerHTML = blackChips;
@@ -155,6 +160,72 @@ openBtn.addEventListener("click", () => {
         frenzytotemText.innerHTML = frenzyTotem;
         countText.innerHTML = count;
         removeText();
+        scrollBar();
+    }
+});
+//抽蘋果開獎
+function prizeDraw(){
+    apples -= 1;
+    appleChips += 1;
+    count += 1;
+    //100.00%
+    let probability = Math.round(Math.random()*10000);
+    if(probability <= 6){
+        //輪迴碑石0.06%
+        frenzyTotem += 1;
+        // console.log("最後獎勵為：" + superbigPrize.name);
+        prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${superbigPrize.name}</span>。</h6>`;
+        alert("恭喜從黃金蘋果機獲得【輪迴碑石】！ 第"+count+"抽");
+    }else if(probability <= 117){
+        //睿智葫蘆1.11%
+        gourd += 1;
+        // console.log("最後獎勵為：" + verybigPrize.name);
+        prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${verybigPrize.name}</span> 道具1個。</h6>`;
+    }else if(probability <= 808){
+        //漆黑碎片(1) 6.91%
+        blackChips += 1;
+        // console.log("最後獎勵為：" + normalbigPrize.name);
+        prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${normalbigPrize.name}</span>。</h6>`;
+    }else if(probability <= 964){
+        //上廣獎 9.64% 採用 普通皮皮計算上廣機率https://www.youtube.com/watch?v=al8tnzHBBo0&ab_channel=%E6%99%AE%E9%80%9A%E7%9A%AE%E7%9A%AE
+        let bigPrizeLength = bigPrize.length-1;
+        let normalP = Math.round(Math.random() * bigPrizeLength);
+        // console.log(normalP);
+        // console.log("最後獎勵為：" + bigPrize[normalP].name);
+        prizeText.innerHTML += `<h6 class="appleBigprizeText" id="Id${textId+=1}">恭喜"你"從黃金蘋果機獲得<span class="textPrize">${bigPrize[normalP].name}</span>。</h6>`;
+    }else if(probability <= 3964){
+        //小獎 30% 航海師裝備
+        let smallPrize2Length = bigPrize.length-1;
+        let smallP2  = Math.round(Math.random() * smallPrize2Length);
+        // console.log("最後獎勵為：" + smallPrize2[smallP2].name);
+        prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${smallPrize2[smallP2].name}</span> 道具1個。</h6>`;
+    }else{
+        //一般獎勵
+        let smallPrizeLength = smallPrize.length-1;
+        let smallP  = Math.round(Math.random() * smallPrizeLength);
+        // console.log("最後獎勵為：" + smallPrize[smallP].name);
+        prizeText.innerHTML += `<h6 class="appleText" id="Id${textId+=1}">已獲得<span class="textPrize">${smallPrize[smallP].name}</span> 道具1個。</h6>`;
+    }
+}
+//一鍵抽
+drawallBtn.addEventListener("click", () => {
+    if(apples === 0){
+        alert("黃金蘋果數量不足，請確認數量是否足夠。");
+    }else{
+        alert("抽取較大的量需要時間計算，請稍等一下『廣頻』及『道具欄』更新。");
+        let k = apples;
+        for(let j=0;j<k;j++){
+            // console.log("j="+j)
+            // console.log("a="+k)
+            prizeDraw();
+            removeText();
+        }
+        applesText.innerHTML = apples;
+        applechipsText.innerHTML = appleChips;
+        blackchipsText.innerHTML = blackChips;
+        gourdText.innerHTML = gourd;
+        frenzytotemText.innerHTML = frenzyTotem;
+        countText.innerHTML = count;
         scrollBar();
     }
 });
